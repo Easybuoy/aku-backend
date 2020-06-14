55cdf8188fedb740f8c624be87c8e729fbf8141e
Ezekiel Ekunola <ekunolaeasybuoy@gmail.com>
2020-06-14 06:55:43 +0100

    setup node api

--- config/db/db.js
+++ config/db/db.js
@@ -1,10 +1,12 @@
-console.log(process.env.DB_URL);
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+console.log(process.env.DB_URL);
 
 const { DB_URL } = process.env;
 //connecting to mongodb
 mongoose
-  .connect(DB_URL, { useNewUrlParser: true })
+  .connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("mongodb connected");
   })
